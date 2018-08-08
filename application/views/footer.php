258a17d4f3cbd4ab07f0411a3383da111d972b7b
Hania Maghfira <if.hania@outlook.com>
2018-08-08 10:31:13 +0700

    Initial commit

---
+++ application/views/footer.php
@@ -0,0 +1,13 @@
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url(); ?>static/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url(); ?>static/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url(); ?>static/js/resume.min.js"></script>
+
+  </body>
+
+</html>
